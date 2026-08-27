fb4958f46b6a1dace29df6c6ce27f85da04ed73a
Ramesh Kumar K <rameshkumar93.68@gmail.com>
2026-08-27 12:51:30 +0530

    Fix pytest module imports with conftest and update remote framework sync

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-class Config:/n    BASE_URL = /https://rameshkumark718.github.io/Chatbot-Automation-Framework///n    GITHUB_REPO_URL = /https://github.com/RameshkumarK718/Chatbot-Automation-Framework//n    EXPLICIT_WAIT = 10/n    IMPLICIT_WAIT = 5/n    BROWSER = /chrome//n    HEADLESS = False/n
+
+class Config:
+
+    BASE_URL = "https://rameshkumark718.github.io/Chatbot-Automation-Framework/"
+
+    GITHUB_REPO_URL = "https://github.com/RameshkumarK718/Chatbot-Automation-Framework"
+
+    EXPLICIT_WAIT = 10
+
+    IMPLICIT_WAIT = 5
+
+    BROWSER = "chrome"
+
+    HEADLESS = False
+
