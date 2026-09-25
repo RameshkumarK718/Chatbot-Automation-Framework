1208e5e1be377f3d5f2da3237eeac25cf7caa7cb
Ramesh Kumar K <rameshkumar93.68@gmail.com>
2026-09-25 15:39:04 +0530

    Update ChatbotTest file with correct locators

---
+++ src/test/java/com/chatbot/ChatbotTest.java
@@ -0,0 +1,33 @@
+
+package com.chatbot;
+
+
+
+import org.testng.annotations.Test;
+
+
+
+public class ChatbotTest {
+
+
+
+    @Test
+
+    public void testChatbotFlow() {
+
+        // Add your test automation steps here
+
+    }
+
+
+
+    private String[] getCredentialsFromExcel() {
+
+        // Placeholder or your credential loading logic
+
+        return new String[] {"user", "password"};
+
+    }
+
+}
+
